4988582129f5ea33f3801078df487110720e715f
vijay Athwal <vjkumar761016@gmail.com>
2021-04-06 17:28:31 +0530

    modal added in cattle page

--- src/components/Cattle.js
+++ src/components/Cattle.js
@@ -1,10 +1,81 @@
 import React from 'react';
 import Layout from '../components/Layout';
 function Cattle() {
+  const [showModal, setShowModal] = React.useState(false);
+
     return (
         <>
           <Layout>
         <div className="mx-auto  max-w-screen-xl ">
+<div className="flex justify-end items-center   h-20">
+        <button
+        className="flex items-center mr-10 bg-blue-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Add Cattle<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+</svg>
+      </button>
+      </div>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center  items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 left-16 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                  Add Cattle
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    
+                  </button>
+                </div>
+                {/*body*/}
+
+                <div className="relative p-6 flex-auto ">
+                  <div className="my-4  text-lg leading-relaxed">
+                  <input type="text" id="email " name="email" className="w-full p-2 bg-transparent focus:border-blue-900 hover:border-green-400 px-8 border-b-2 text-black border-blue-700  outline-none "/>
+                  </div>
+                  <div className="my-4 text-lg leading-relaxed">
+                  <input type="text" id="email " name="email" className="w-full p-2 bg-transparent focus:border-blue-900 hover:border-green-400 px-8 border-b-2 text-black border-blue-700   outline-none "/>
+                  </div>
+                  <div className="my-4  text-lg leading-relaxed">
+                  <input type="text" id="email " name="email" className="w-full p-2 bg-transparent focus:border-blue-900 hover:border-green-400 px-8 border-b-2 text-black border-blue-700   outline-none "/>
+                  </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-blue-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+              
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+
             <div className="flex flex-col w-11/12 mx-auto py-5">
             
   <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
