0eeacd437368e5069f3653d16f3632b6c40f722d
devnasim <devnasim18@gmail.com>
2021-04-14 00:00:19 +0530

    minor changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client/react';
 import { Home, Users, Login, Profile, Cattle, Categories } from './pages';
 import client from './utils/client';
+
+// eslint-disable-next-line react/prop-types
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isLoggedIn = window.localStorage.getItem('token');
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (!isLoggedIn) {
+          return <Redirect to="/login" />;
+        }
+        return <Component {...props} />;
+      }}
+    />
+  );
+};
 
 const App = () => (
   <ApolloProvider client={client}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/" component={Home} />
+        <PrivateRoute exact path="/login" component={Login} />
         <Route exact path="/users" component={Users} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/cattle" component={Cattle} />
