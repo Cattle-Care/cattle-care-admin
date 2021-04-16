82822519ac821dfa57e8ecf4168c545247683386
devnasim <devnasim18@gmail.com>
2021-04-16 23:29:14 +0530

    minor fixes

--- src/pages/Cattle.js
+++ src/pages/Cattle.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Formik } from 'formik';
+
 import { gql, useMutation } from '@apollo/client';
 import { Pager } from '../components/elements';
 import Layout from '../components/global/Layout';
+import AddCattleForm from '../components/AddCattleForm';
 
 const addCattleMutation = gql`
   mutation addCattle($input: CattleInput!) {
@@ -15,12 +16,12 @@ const addCattleMutation = gql`
 `;
 
 // eslint-disable-next-line react/prop-types
-function Cattle({ onSubmit }) {
+function Cattle() {
   const [showModal, setShowModal] = React.useState(false);
 
-  const [loginAction] = useMutation(addCattleMutation, {
-    onCompleted: ({ added }) => {
-      console.log('added', added);
+  const [addCattleAction, { loading, error }] = useMutation(addCattleMutation, {
+    onCompleted: ({ addCattle }) => {
+      console.log('addCattle', addCattle);
     },
   });
 
@@ -70,62 +71,11 @@ function Cattle({ onSubmit }) {
 
                     <div className="relative p-6 flex-auto ">
                       <div className="my-4  text-lg leading-relaxed">
-                        <Formik
-                          initialValues={{ name: '', age: '', category: '', weight: '' }}
-                          validate={values => {
-                            const errors = {};
-                            if (!values.name) {
-                              errors.name = 'Required';
-                            }
-                            return errors;
+                        <AddCattleForm
+                          onSubmit={values => {
+                            addCattleAction({ variables: { input: {} } });
                           }}
-                        >
-                          {({ values, handleChange, handleBlur, handleSubmit }) => (
-                            <form
-                              onSubmit={event => {
-                                event.preventDefault();
-                                handleSubmit();
-                              }}
-                            >
-                              <div className="my-2 text-gray-400">Name</div>
-                              <input
-                                type="text"
-                                name="name"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.name}
-                                className="w-full p-2 bg-transparent focus:border-b-4 focus:border-blue-900 hover:border-green-400 px-8 border-b-2 text-black border-blue-700  outline-none "
-                              />
-                              <div className="my-2 text-gray-400">Age</div>
-                              <input
-                                type="text"
-                                name="age"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.age}
-                                className="w-full p-2 bg-transparent focus:border-blue-900 hover:border-green-400 px-8 border-b-2 text-black border-blue-700   outline-none "
-                              />
-                              <div className="leading-10 text-gray-400">Category</div>
-                              <input
-                                type="text"
-                                name="category"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.category}
-                                className="w-full p-2  bg-transparent focus:border-blue-900 hover:border-green-400 px-8 border-b-2 text-black border-blue-700   outline-none "
-                              />
-                              <div className=" text-gray-400">Weight</div>
-                              <input
-                                type="text"
-                                name="weight"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.weight}
-                                className="w-full p-2 bg-transparent focus:border-blue-900 hover:border-green-400 px-8 border-b-2 text-black border-blue-700   outline-none "
-                              />
-                            </form>
-                          )}
-                        </Formik>
+                        />
                       </div>
                     </div>
                     {/* footer */}
